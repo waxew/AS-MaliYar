93c6fc5e53ac6bf9a767a03367e0336ea5f41d76
dreautall <109872040+dreautall@users.noreply.github.com>
2025-11-29 21:28:54 +0100

    fix sharing to app with latest `flutter_sharing_intent` version

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_sharing_intent
 
 @main @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
@@ -11,5 +12,16 @@ import UIKit
 
     func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
         GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    }
+
+    override func application(_ app: UIApplication, openurl: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        let sharingIntent = SwiftFlutterSharingIntentPlugin.instance
+        /// if the url is made from SwiftFlutterSharingIntentPlugin then handle it with plugin [SwiftFlutterSharingIntentPlugin]
+        if sharingIntent.hasSameSchemePrefix(url: url) {
+            return sharingIntent.application(app, open: url, options: options)
+        }
+
+        // Proceed url handling for other Flutter libraries like uni_links
+        return super.application(app, open: url, options: options)
     }
 }
